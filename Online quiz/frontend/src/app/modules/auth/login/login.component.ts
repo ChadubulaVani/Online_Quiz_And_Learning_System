77427b79ccce611753efb21f84f4a66443c5fda6
ChadubulaVani <chadubula.vani@gmail.com>
2026-03-29 17:43:05 +0530

    Update login.component.ts

--- Online quiz/frontend/src/app/modules/auth/login/login.component.ts
+++ Online quiz/frontend/src/app/modules/auth/login/login.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
